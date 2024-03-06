329bfa01a51b7c31e9cd647310aff2c1a2f6e3c1
Hashan <hemachandra.hashan@gmail.com>
2024-03-06 06:37:17 +0530

    seperate divs

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="flex flex-row gap-4 justify-center items-center w-screen h-screen text-center">
+      <div className="w-96 h-96 bg-slate-100/90 rounded-sm">Input</div>
+      <div className="w-96 h-96 bg-slate-100/90 rounded-sm">Preview</div>
+    </div>
+  );
+};
 
-export default page
+export default page;
